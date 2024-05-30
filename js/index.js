fbb2037ad4b8cecc8e05c941197de102284d72e5
DuyNguyen <duyyb257946@gmail.com>
2024-05-30 15:29:01 +0700

    remove rotate

--- js/index.js
+++ js/index.js
@@ -1,16 +1,6 @@
-const BoardController = {
-  init: function (obj, callback) {
-    let self = this;
-    self.activeBoard = obj;
-    setupEventGroupElement();
-    setupEventCycleElement();
-    setupEventGroupCategory();
-    renderElement();
-  },
-};
-
 document.addEventListener("DOMContentLoaded", function (event) {
-  boardDom = document.getElementById("body");
-  boardController = Object.create(BoardController);
-  boardController.init(boardDom);
+  setupEventGroupElement();
+  setupEventCycleElement();
+  setupEventGroupCategory();
+  renderElement();
 });
